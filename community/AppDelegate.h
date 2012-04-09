31b9dda1ea75bdcd34dc0833e0d77ad7956f3371
Cristinita y Andrésin <stoyhot@MacBook-Pro-de-Jessica-leon.local>
2012-04-08 21:22:46 -0500

    Initial Commit

---
+++ community/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  community
+//
+//  Created by Cristina leon on 08/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
