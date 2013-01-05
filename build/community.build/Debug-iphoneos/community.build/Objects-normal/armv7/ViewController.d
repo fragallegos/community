271c53d51a04a398534fdb51157e332bac317eb6
fragallegos <fragallegos@me.com>
2013-01-04 20:30:05 -0500

    UI Community

---
+++ build/community.build/Debug-iphoneos/community.build/Objects-normal/armv7/ViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/stoyhot/Documents/app\ para\ comercializar\ y\ licencia\ contrato/music/usfqmapa3D/CommunityUI/community/community/ViewController.m \
+  /Users/stoyhot/Documents/app\ para\ comercializar\ y\ licencia\ contrato/music/usfqmapa3D/CommunityUI/community/community/ViewController.h
---
+++ build/community.build/Debug-iphoneos/community.build/Objects-normal/armv7s/main.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/stoyhot/Documents/app\ para\ comercializar\ y\ licencia\ contrato/music/usfqmapa3D/CommunityUI/community/community/main.m \
+  /Users/stoyhot/Documents/app\ para\ comercializar\ y\ licencia\ contrato/music/usfqmapa3D/CommunityUI/community/community/AppDelegate.h
